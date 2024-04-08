295f31e78610eeba65ac05e12a8b109b6d131aba
polin <polina.pak2807@gmail.com>
2024-04-08 20:23:38 +0300

    удален коммент

--- NoteAppUI/Program.cs
+++ NoteAppUI/Program.cs
@@ -8,9 +8,7 @@ namespace NoteAppUI
 {
     internal static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
+      
         [STAThread]
         static void Main()
         {
